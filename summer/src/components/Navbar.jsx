b023a396e1d03c89799f0c54874052d32a553fa4
eStella_ <75822584+doot00@users.noreply.github.com>
2025-11-07 07:04:28 +0900

    firebase 추가
    .

--- summer/src/components/Navbar.jsx
+++ summer/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import { LuSquareMenu } from "react-icons/lu";
 import '../App.css'
+import { login } from "../api/firebase";
+
 
 export default function Navbar() {
     return (
@@ -17,7 +19,7 @@ export default function Navbar() {
                 <Link to="/allproducts">Products</Link>
                 <Link to="/carts">Carts</Link>
                 <Link to="/products/new">New</Link>
-                <button className="p-1 bg-red-500 text-white rounded-2xl">Login</button>
+                <button onClick={login}className="p-1 bg-red-500 text-white rounded-2xl">Login</button>
             </nav>
         </header>
     )
