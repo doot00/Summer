19a080344f599cf5c2d5d55d30b75eb51a0703fa
eStella_ <75822584+doot00@users.noreply.github.com>
2025-11-27 10:22:51 +0900

    ProductDetail 추가
    .

--- summer/src/pages/ProductCard.jsx
+++ summer/src/pages/ProductCard.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom"
 
-export default function ProductCard({product: {id, image, title, options, price },}) {
+export default function ProductCard({product, product: {id, image, title, options, price },}) {
+    const navigate = useNavigate();
     return (
         <>
-            <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+            <li onClick={() => {navigate(`/products/${id}`, {state: { product }})}}className="rounded-lg shadow-md overflow-hidden cursor-pointer">
                 <img className='w-full h-[400px] p-10 relative z-0 transition-all duration-300 hover:scale-110' style={{ backgroundColor: "#f7f5f6" }} src={image} alt={title} />
                 <div className="m-3">
                     <h3 className="text-lg mb-1">{title}</h3>
--- summer/src/pages/ProductDetail.jsx
+++ summer/src/pages/ProductDetail.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
 export default function ProductDetail() {
-    return (
-        <>
-            <h1>ProductDetail</h1>
-        </>
-    )
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(category);
+  const handleSelect = (e) => setSelected(e.target.value);
+  const handleClick = (e) => {
+    // 여기 장바구니에 추가하면 된다.
+  };
+  return (
+    <>
+      <section className="flex flex-col justify-center py-20 md:flex-row p-4">
+        <img className="w-[300px] px-4 basis-5/12 bg-cover" src={image} alt={title} />
+        <div className="w-full basis-5/12 flex flex-col p-4" style={{ fontFamily: "proSlim_regular, sans-serif" }}>
+          <h2 className="text-3xl mb-2 font-bold py-2 border-b border-gray-400">{title}</h2>
+          <p className="m-2 p-2 bg-gray-100 rounded-xl text-xl font-bold">{price.toLocaleString()}원</p>
+          <p className="m-2 p-2 bg-gray-100 rounded-xl">{description}</p>
+            <div
+              className="flex items-center m-2 p-2 bg-gray-100 rounded-xl"
+            >category : {category}
+            </div>
+
+          <div className="flex justify-center items-center mb-10 mt-5">
+            <button
+              className="bg-red-500 rounded-2xl p-2 w-[200px] text-white font-bold"
+              style={{ fontFamily: "proSlim_regular, sans-serif" }}
+              onClick={handleClick}
+            >
+              장바구니 추가
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
