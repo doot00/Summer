4bfa574d5ed7f28d48fffb58301ba84ca14fbf8d
eStella_ <75822584+doot00@users.noreply.github.com>
2025-12-02 19:53:01 +0900

    cart css 수정
    cart css 수정

--- summer/src/components/CartItem.jsx
+++ summer/src/components/CartItem.jsx
@@ -15,18 +15,18 @@ export default function CartItem({product, product: {id, image, title, category,
     const handleDelete = () => removeFromCart(uid, id);
     return (
         <>
-            <li className="flex justify-between items-center p-10 border rounded-xl p-2 m-2">
-                <input className="mx-5" type="checkbox" />
+            <li className="flex justify-between items-center p-10 border rounded-xl p-2 m-2 mt-10">
+                <input className="mx-5 scale-150" type="checkbox" />
                 <div className="flex-1 flex justify-between items-center mx-5">
-                <img className='w-24 md:w-48 rounded-lg mx-5'src={image} alt={title} />
+                <img className='w-24 md:w-48 rounded-lg mx-5 mt-2'src={image} alt={title} />
                     <div className="basis-3/5 mx-10">
-                        <p className="text-lg">{title}</p>
-                        <p className="text-xl">{category}</p>
+                        <p className="text-xl">{title}</p>
+                        <p className="text-lg">{category}</p>
                         <p className="text-xl font-bold">{price.toLocaleString()}</p>
                     </div>
-                    <div className="text-xl flex items-center mx-2 mr-10">
+                    <div className="text-xl flex items-center mx-2 mr-10 border rounded-lg">
                         <FaMinus className={ICON_CLASS} onClick={handleMinus}/>
-                        <span className="text-2xl mx-2">{quantity}</span>
+                        <span className="text-xl px-3 py-1 mb-1 border-l border-r">{quantity}</span>
                         <FaPlus className={ICON_CLASS} onClick={handlePlus}/>
                     </div>
                     <div className="text-xl mx-5 flex items-center">
--- summer/src/pages/MyCart.jsx
+++ summer/src/pages/MyCart.jsx
@@ -4,37 +4,43 @@ import { useAuthContext } from "../context/AuthContext";
 import CartItem from "../components/CartItem";
 
 const SHIPPING = 3500;
-export default function MyCart({ }) {
+export default function MyCart() {
     const { uid } = useAuthContext();
     const { isLoading, data: products } = useQuery({ queryKey: ['carts', uid], queryFn: () => getCart(uid), enabled: !!uid,});
     
     if(isLoading) return <p>Loading...</p>
     const hasProducts = products && products.length > 0; // 쇼핑카트의 아이템이 1이상일 경우
     const totalPrice = products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0);
+    
     return (
         <section>
             <h1 className="flex justify-center items-center m-10 text-4xl" style={{ fontFamily: "Cafe24Meongi, sans-serif" }}>
                 Carts
             </h1>
-
-            {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
-            {hasProducts && (
-                <section className="flex justify-between items-center mb-10 p-20">
-                    <ul className="w-4/6 p-5 border rounded-2xl">
-                        <input className="text-2xl" type="checkbox" /> 전체상품주문
-                        {products && products.map((product) => (
-                        <CartItem key={product.id} product={product} uid={uid}/>))}
-                        <p className="flex border rounded-xl bg-gray-100 p-2 m-2 justify-center text-xl">상품 금액 {totalPrice.toLocaleString()} + 배송비 3,500 = 상품총금액 {(totalPrice+SHIPPING).toLocaleString()}</p>
-                    </ul>
-                    <div className="h-1/6 justify-center border rounded-2xl p-10 text-xl">
-                        <h2 className="text-2xl mb-2">결제예정금액</h2>
-                        <p className="mb-2">상품 금액 : {totalPrice.toLocaleString()}</p>
-                        <p className="mb-2">배송액 : 3,500</p>
-                        <p className="mb-2">수량 : {products.quantity}</p>
-                        <p className="">상품총금액 : {(totalPrice+SHIPPING).toLocaleString()}</p>
-                    </div>
-                </section>
-            )}
+            <div style={{ fontFamily: "proSlim_regular, sans-serif" }}>
+                {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
+                {hasProducts && (
+                    <section className="flex justify-between items-center mb-10 p-10 gap-x-5">
+                        <ul className="w-5/6 p-5 border rounded-2xl">
+                            <li className="flex text-2xl m-5">
+                                <input className="scale-150" type="checkbox" />
+                                <p className="flex ml-5">전체상품주문</p>
+                            </li>
+                            {products && products.map((product) => (
+                            <CartItem key={product.id} product={product} uid={uid}/>))}
+                            <p className="flex border rounded-xl bg-gray-100 p-2 m-2 mt-5 justify-center text-xl">상품 금액 {totalPrice.toLocaleString()} + 배송비 3,500 = 상품총금액 {(totalPrice+SHIPPING).toLocaleString()}</p>
+                        </ul>
+                        <div className="h-1/6 justify-between border rounded-2xl p-8 text-lg">
+                            <h2 className="text-2xl mb-2">결제예정금액</h2>
+                            <p className="mb-2">상품 금액 : {totalPrice.toLocaleString()}</p>
+                            <p className="mb-2">배송액 : 3,500</p>
+                            <p className="mb-2">수량 : </p>
+                            <p className="mb-5">상품총금액 : {(totalPrice+SHIPPING).toLocaleString()}</p>
+                            <button className="bg-red-500 rounded-2xl p-2 w-[200px] text-white font-bold">주문하기</button>
+                        </div>
+                    </section>
+                )}
+            </div>
         </section>
     )
 }
